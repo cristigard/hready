725bacbb6ef675261427043596f632817ed49e10
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2022-03-31 12:14:20 +0300

    Add template test, data base test

--- hosts/tests.py
+++ hosts/tests.py
@@ -1,2 +1,17 @@
 from django.test import TestCase
+from .models import CityModel, ReservationModel
+from decimal import Decimal
 
+
+
+class UnitTestCase(TestCase):
+
+	def test_template(self):
+		response = self.client.get('/upload/')
+		self.assertTemplateUsed(response, 'hosts/upload_file.html')
+
+	def test_dbs(self):
+		CityModel.objects.create(city="CLUJ", commission_percent = Decimal(0.1))
+		ReservationModel.objects.create(reservation='HH001', checkin='2020-10-10', 
+			                            city='CLUJ', checkout= '2020-10-15', flat= "Jamal", income=Decimal(1000))
+		self.assertEqual(ReservationModel.objects.filter(city="CLUJ").first().commission, Decimal(100))
